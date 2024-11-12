518b79c357c010c938e6d05cc9bc6eb810e1d53b
Firza Irvan <94727919+firzzairvn@users.noreply.github.com>
2024-11-12 12:01:40 +0700

    Update coderunner.py

--- coderunner.py
+++ coderunner.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 from langchain_experimental.tools import PythonREPLTool
 from langchain_experimental.utilities import PythonREPL
 
-openai_api_key = "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"
+openai_api_key = ""
 
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
